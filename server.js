36d5080fe4db802623a54e03677fd1110bd96ff5
StevanFreeborn <sfreeborn7@gmail.com>
2022-05-09 11:20:52 -0500

    begin implementing file info endpoint

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const EXPRESS = require('express');
 const CORS = require('cors');
+const MULTER = require('multer');
+const UPLOAD = MULTER({ dest: 'uploads/' });
 
 const APP = EXPRESS();
 
@@ -8,6 +10,15 @@ APP.use(CORS());
 
 // serve static files from public directory
 APP.use(EXPRESS.static(`public`));
+
+APP.get("/", (req, res) => {
+    res.sendFile(`${process.cwd()}/views/index.html`);
+});
+
+APP.post('/api/fileinfo', UPLOAD.single('upfile'), (req, res) => {
+    console.log(req.file.originalname);
+    res.send('hello');
+});
 
 const LISTENER = APP.listen(process.env.PORT || 3000, () => {
     console.log(`Listening on port ${LISTENER.address().port}`);
